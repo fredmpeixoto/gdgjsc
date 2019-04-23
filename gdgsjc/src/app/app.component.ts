16bc726e60eaa6772d3b66e7011092d3163b7788
Frederico Lima <frederico.lima@laureate.com.br>
2019-04-23 16:11:04 -0300

    add efect for load the page

--- gdgsjc/src/app/app.component.ts
+++ gdgsjc/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'gdgsjc';
+  telaInteira: boolean;
+
+  ngOnInit(): void {
+    this.mostrarTelaInteira();
+  }
+
+  mostrarTelaInteira(){
+    setTimeout(() => {
+    this.telaInteira = true;
+    }, 1000);
+  }
+
 }
