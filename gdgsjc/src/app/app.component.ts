a056a876384cc82731a2a98808855a69a37daaf2
Frederico Lima <frederico.lima@laureate.com.br>
2019-04-25 10:24:42 -0300

    effects icons

--- gdgsjc/src/app/app.component.ts
+++ gdgsjc/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  title = 'gdgsjc';
   telaInteira: boolean;
+  logos: string[];
+  moveFacebook: boolean;
+  moveInstagram: boolean;
+  moveLinkedin: boolean;
+  moveMeetup: boolean;
+  moveEmail: boolean;
+
+  constructor() {
+    this.logos = ['facebook', 'meetup', 'instagram', 'linkedin', 'email'];
+
+  }
 
   ngOnInit(): void {
     this.MostrarTelaInteira();
+    this.MoveIcon();
   }
 
-  MostrarTelaInteira(){
+  MostrarTelaInteira() {
     setTimeout(() => {
-    this.telaInteira = true;
+      this.telaInteira = true;
     }, 1000);
   }
 
-  RedirectUrl(url:string): void{
+  MoveIcon(): void {
+    setTimeout(() => {
+      this.moveFacebook = !this.moveFacebook;
+    }, 2000);
+
+    setTimeout(() => {
+      this.moveFacebook = !this.moveFacebook;
+      this.moveInstagram = !this.moveInstagram;
+    }, 3000);
+
+
+    setTimeout(() => {
+      this.moveFacebook = this.moveFacebook;
+      this.moveInstagram = !this.moveInstagram;
+      this.moveLinkedin = !this.moveLinkedin;
+    }, 4000);
+
+    setTimeout(() => {
+      this.moveFacebook = this.moveFacebook;
+      this.moveInstagram = this.moveInstagram;
+      this.moveLinkedin = !this.moveLinkedin;
+      this.moveMeetup = !this.moveMeetup;
+    }, 5000);
+
+    setTimeout(() => {
+      this.moveFacebook = this.moveFacebook;
+      this.moveInstagram = this.moveInstagram;
+      this.moveLinkedin = this.moveLinkedin;
+      this.moveMeetup = !this.moveMeetup;
+      this.moveEmail = !this.moveEmail;
+    }, 6000);
+
+    setTimeout(() => {
+      this.moveEmail = !this.moveEmail;
+    }, 7000);
+
+
+  }
+
+  RedirectUrl(url: string): void {
     window.open(url);
   }
 
-  SendEmail(email:string): void{
+  SendEmail(email: string): void {
     window.location.href = email;
   }
 
