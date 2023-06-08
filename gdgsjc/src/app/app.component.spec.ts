dcb5adeeff87d1dc88acb9a09543529cdfff07e4
Fred Peixoto <fredmpeixoto@live.com>
2023-06-08 12:37:05 -0300

    Updated PWA

--- gdgsjc/src/app/app.component.spec.ts
+++ gdgsjc/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gdgsjcc'`, () => {
+  it(`should have as title 'gdgsjc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gdgsjcc');
+    expect(app.title).toEqual('gdgsjc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gdgsjcc app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gdgsjc app is running!');
   });
 });
--- gdgsjc/src/app/app.module.ts
+++ gdgsjc/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDividerModule } from "@angular/material/divider";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,13 @@ import { MatDividerModule } from "@angular/material/divider";
     MatInputModule,
     MatButtonModule,
     MatDividerModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
